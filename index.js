f78581f8523001d07225f3cdf26c02c39467bf63
thekiwidev <adedotungabriel20@gmail.com>
2022-06-13 20:16:24 +0100

    done with the joke API, and the categoty selector, to work next on choosing from 'two-part' or 'single'

---
+++ index.js
@@ -0,0 +1,29 @@
+let url = "https://v2.jokeapi.dev/joke/Any";
+
+function getJoke(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      let joke = data;
+      document.querySelector(".pickup_line").innerHTML = joke.setup;
+      document.querySelector(".delivery_line").innerHTML = joke.delivery;
+    })
+    .catch((err) => console.error(err));
+}
+getJoke(url);
+
+// let's infiltrate the DOM
+const btn = document.querySelector("form button");
+
+const category = document.getElementById("category");
+
+category.addEventListener("change", () => {
+  url = `https://v2.jokeapi.dev/joke/${category.value}`;
+  console.log(url);
+  getJoke(url);
+});
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  getJoke(url);
+});
